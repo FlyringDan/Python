02d32047c22007a2019a708bfe425fd11bddb349
FlyringDan <148276602+FlyringDan@users.noreply.github.com>
2023-11-18 19:54:41 +0300

    Add files via upload

--- money_capital.py
+++ money_capital.py
@@ -5,10 +5,10 @@ increase = 0.03  # Ежемесячный рост цен
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
-money_capital = spend - salary
+money_capital = 0
 
-for i in range(months - 1):
-    money_capital += (spend * increase) + spend - salary
+for _ in range(months):
+    money_capital += spend - salary
     spend = spend * increase + spend
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital, 2))
